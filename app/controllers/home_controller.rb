9119dac1e40b3ada8c45582606dfc760e19cb405
Zersiax <florianbeijers@gmail.com>
2017-01-17 13:21:10 +0000

    Added controller, route, static page and heading, I think I made it blue :P

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController # Home is a common name for static pages controllers, so are welcome and staticPage. I like home and since I don't know where this thing is going in the future it seemed like a sensible convention
+def index
+
+end
+end
